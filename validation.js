637525e177bab5d857cff7cc7cd27783e11afd99
Vaishnavi Jariwala <jariwalavaishnavi52@gmail.com>
2021-09-24 19:15:32 +0530

    css added

--- validation.js
+++ validation.js
@@ -20,3 +20,18 @@ function emailvalidator(){
         alert("This is not a valid email id");
     }
 }
+function calculateAge(){
+    var user_date=document.getElementById("DOB").value;
+    var dob = new Date(user_date);  
+    var dif_month = Date.now() - dob.getTime();  
+    var month_date_format = new Date(dif_month);       
+    var year = month_date_format.getUTCFullYear();  
+    var user_age = Math.abs(year - 1970);   
+    document.getElementById("age").value = user_age;  
+}
+function validatingAge(){
+    var age=document.getElementById("age").value;
+    if(age<18){
+        alert("Age of donor can't be less than 18 years.");
+    }
+}
